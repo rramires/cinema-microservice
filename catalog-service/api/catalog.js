7f9b18866982012284ae034d4015a9ab79d076c6
rramires <ricardo@flexbr.com>
2023-01-02 20:50:19 -0300

    Add catalog routes

--- catalog-service/api/catalog.js
+++ catalog-service/api/catalog.js
@@ -1,29 +1,33 @@
+
+
 /**
  * Export function for dependency inversion betwin app and repository
  * connection on runtime execute: server->start
  */
 module.exports = (app, repository) => {
 
+    const repository = require('../repository/repository');
+
     /**
-     * Get Premiere Movies
+     * Get Cities
      */
-    app.get('/movies/premieres', async(req, res, next) => {
+    app.get('/cities', async(req, res, next) => {
         // get 
-        const movies = await repository.getPremiereMovies();
+        const cities = await repository.getAllCities();
         // response
-        res.json(movies);
+        res.json(cities);
     });
 
 
     /**
-     * Get Movie by ID
+     * Get Movies by city ID
      */
-    app.get('/movies/:id', async(req, res, next) => {
+    app.get('/cities/:cityId/movies', async(req, res, next) => {
         // get 
-        const movie = await repository.getMovieById(req.params.id);
+        const movies = await repository.getMoviesByCityId(req.params.cityId);
         // test
-        if(movie){
-            res.json(movie);
+        if(movies){
+            res.json(movies);
         }
         else{
             // status not found
@@ -33,12 +37,69 @@ module.exports = (app, repository) => {
 
 
     /**
-     * Get ALL Movies
+     * Get Sessions by city and movie ID
      */
-    app.get('/movies', async(req, res, next) => {
+    app.get('/cities/:cityId/movies/:movieId', async(req, res, next) => {
         // get 
-        const movies = await repository.getAllMovies();
-        // response
-        res.json(movies);
+        const sessions = await repository.getMovieSessionByCinemaId(req.params.movieId, req.params.cityId);
+        // test
+        if(sessions){
+            res.json(sessions);
+        }
+        else{
+            // status not found
+            res.sendStatus(404);
+        }
+    });
+
+
+    /**
+     * Get Cinemas by city ID
+     */
+    app.get('/cities/:cityId/cinemas', async(req, res, next) => {
+        // get 
+        const cinemas = await repository.getCinemasByCityId(req.params.cityId);
+        // test
+        if(cinemas){
+            res.json(cinemas);
+        }
+        else{
+            // status not found
+            res.sendStatus(404);
+        }
+    });
+
+
+    /**
+     * Get Movies by cinema ID
+     */
+    app.get('/cinemas/:cinemaId/movies', async(req, res, next) => {
+        // get 
+        const movies = await repository.getMoviesByCinemaId(req.params.cinemaId);
+        // test
+        if(movies){
+            res.json(cinemas);
+        }
+        else{
+            // status not found
+            res.sendStatus(404);
+        }
+    });
+
+
+    /**
+     * Get Movies by cinema ID
+     */
+    app.get('/cinemas/:cinemaId/movies/:movieId', async(req, res, next) => {
+        // get 
+        const sessions = await repository.getMovieSessionByCinemaId(req.params.movieId, req.params.cinemaId);
+        // test
+        if(sessions){
+            res.json(sessions);
+        }
+        else{
+            // status not found
+            res.sendStatus(404);
+        }
     });
 };
